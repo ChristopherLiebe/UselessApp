f26cd312f74eae9c75237415579fac976de8bd01
Christopher Liebe <carlchristopherliebe@gmail.com>
2021-08-31 10:06:14 -0700

    Added destruct button, and green switch, also made the destruct button no longer functional, and lastly setup the look busy button layout.

--- app/src/main/java/com/example/uselessapp/MainActivity.kt
+++ app/src/main/java/com/example/uselessapp/MainActivity.kt
@@ -1,15 +1,14 @@
 package com.example.uselessapp
 
+import android.annotation.SuppressLint
 import android.graphics.Color
-import android.media.Rating
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.os.CountDownTimer
-import android.widget.Button
-import android.widget.RatingBar
-import android.widget.Switch
-import android.widget.Toast
+import android.widget.*
 import androidx.constraintlayout.widget.ConstraintLayout
+import androidx.constraintlayout.widget.ConstraintSet.INVISIBLE
+import androidx.constraintlayout.widget.ConstraintSet.VISIBLE
 import java.util.*
 import kotlin.concurrent.schedule
 
@@ -20,6 +19,10 @@ class MainActivity : AppCompatActivity() {
     lateinit var buttonLookBusy : Button
     lateinit var rating : RatingBar
     lateinit var background : ConstraintLayout
+    lateinit var cancelButton : Button
+    lateinit var finishButton : Button
+    lateinit var progressBar : ProgressBar
+    lateinit var progressBarCaption : TextView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -29,6 +32,7 @@ class MainActivity : AppCompatActivity() {
 
         switch.setOnCheckedChangeListener { buttonView, isChecked ->
             if(isChecked) {
+                Toast.makeText(MainActivity@this, "Switch goes on", Toast.LENGTH_SHORT).show()
                 background.setBackgroundColor(Color.argb(100, 0, 255, 0))
                 startSwitchTimer()
             }
@@ -36,22 +40,74 @@ class MainActivity : AppCompatActivity() {
                 Toast.makeText(MainActivity@this, "Switch goes off", Toast.LENGTH_SHORT).show()
                 background.setBackgroundColor(Color.argb(100, 255, 255, 255))
             }
-
         }
+
+        buttonSelfDestruct.setOnClickListener() {
+            startDestructTimer()
+        }
+
+        buttonLookBusy.setOnClickListener() {
+            lookBusyScreen()
+        }
+    }
+
+    @SuppressLint("WrongConstant")
+    private fun lookBusyScreen() {
+        switch.visibility = INVISIBLE
+        buttonSelfDestruct.visibility = INVISIBLE
+        buttonLookBusy.visibility = INVISIBLE
+        rating.visibility = INVISIBLE
+        cancelButton.visibility = VISIBLE
+        finishButton.visibility = VISIBLE
+        progressBar.visibility = VISIBLE
+        progressBarCaption.visibility = VISIBLE
 
     }
 
     private fun startSwitchTimer() {
-        object : CountDownTimer(3000, 1000) {
-            override fun onTick(p0: Long) {
-                
+        // make an anonymous inner class to create a CountDownTimer object
+        val uselessTimer = object : CountDownTimer(3000, 500) {
+            // callbacks -- functions that will be called later, sometime in the future
+            override fun onTick(millisUntilFinished: Long) {
+
             }
 
             override fun onFinish() {
-                TODO("Not yet implemented")
+                // Display the number of seconds remaining on the button
+                // to close the activity: finish()
+                switch.isChecked = false
+            }
+        }
+        uselessTimer.start()
+    }
+
+    private fun startDestructTimer() {
+        var isRed = false
+        var flashDuration = 500
+        var flashTimeout = 0
+
+        val uselessTimer = object : CountDownTimer(10000, 1) {
+            override fun onTick(millisUntilFinished: Long) {
+                flashTimeout++
+                if(flashDuration <= flashTimeout) {
+                    if (!isRed) {
+                        background.setBackgroundColor(Color.argb(200, (((Math.random())*155)+ 100).toInt(), 0, 0))
+                    }
+                    else {
+                        background.setBackgroundColor(Color.argb(100, 255, 255, 255))
+                    }
+                    flashDuration -= 50
+                    flashTimeout = 0
+                }
+                buttonSelfDestruct.text = "${millisUntilFinished}"
             }
 
+            override fun onFinish() {
+                finish()
+            }
         }
+
+        uselessTimer.start()
     }
 
     private fun wireWidgets() {
@@ -60,5 +116,10 @@ class MainActivity : AppCompatActivity() {
         buttonLookBusy = findViewById(R.id.buttonBusy)
         rating = findViewById(R.id.ratingBar)
         background = findViewById(R.id.constraintLayout)
+        cancelButton = findViewById(R.id.cancelButton)
+        finishButton = findViewById(R.id.finishButton)
+        progressBar = findViewById(R.id.progressBar)
+        progressBarCaption = findViewById(R.id.progressBarCaption)
+
     }
 }
